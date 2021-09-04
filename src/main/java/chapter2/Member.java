29a17d548f38785b50f7adb0a68136f23a16b784
junheejang <gggujunhee@gmail.com>
2021-09-04 15:48:33 +0900

    feat: chapter4

--- src/main/java/chapter2/Member.java
+++ src/main/java/chapter2/Member.java
@@ -1,19 +1,37 @@
 package chapter2;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
-@Table(name = "MEMBER")
+@Table(name = "MEMBER", uniqueConstraints = {
+        @UniqueConstraint(
+                name = "NAME_AGE_UNIQUE",
+                columnNames = {"NAME", "AGE"}
+        )
+})
 public class Member {
 
     @Id
     @Column(name = "ID")
     private String id;
 
-    @Column(name = "NAME")
+    @Column(name = "NAME", nullable = false, length = 10)
     private String userName;
 
     private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
 
     public String getId() {
         return id;
